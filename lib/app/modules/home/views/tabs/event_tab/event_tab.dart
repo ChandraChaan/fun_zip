54eb9ff9398c4691b35682fde96c6d653c7bebc1
Chandra <dchandrachaan@gmail.com>
2023-10-14 09:18:27 +0530

    optimize code and added responsive ui

--- lib/app/modules/home/views/tabs/event_tab/event_tab.dart
+++ lib/app/modules/home/views/tabs/event_tab/event_tab.dart
@@ -23,121 +23,145 @@ class EventTab extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO NestedScrollView I am removed here, if needed do add it
-    // SingleChildScrollView do add
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Obx(() {
-            return AppBarWidget(
-                color: backgroundColor,
-                title: '${controller.userAddress.value.locality ?? ''}');
-          }),
-          // SearchEventDialog(),
-          InkWell(
-            onTap: () {
-              Get.dialog(SearchEventDialog());
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FormBuilderTextField(
-                enabled: false,
-                name: 'search',
-                // controller: controller.emailTextEditingController,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                keyboardType: TextInputType.emailAddress,
-                autofillHints: [AutofillHints.name],
-                decoration: InputDecoration(
-                  hintMaxLines: 2,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  filled: true,
-                  hintStyle: normalText.copyWith(height: 1.2
-                    //You can set your custom height here
+    return GetX(
+      init: HomeController(),
+      builder: (controller) {
+        if (controller.loading.value == true) {
+          return responsiveWidget(
+            isScroll: false,
+            child: bodyWidget(),
+          );;
+        } else {
+          if (controller.apiResponseModel.value.results == null) {
+            return responsiveWidget(
+              isScroll: false,
+              child: bodyWidget(),
+            );;
+          } else {
+            return responsiveWidget(
+              isScroll: true,
+              child: bodyWidget(),
+            );
+          }
+        }
+      },
+    );
+  }
 
-                  ),
-                  hintText: "Search All events...\nWhere | When | Category",
-                  fillColor: Colors.white,
-                  prefixIcon: IconButton(
-                      onPressed: null,
-                      icon: SvgPicture.asset(SearchIconTextFieldIconImage)),
-                  suffixIcon: IconButton(
-                      onPressed: null,
-                      icon: SvgPicture.asset(filterTextFieldIconImage)),
-                  prefixIconConstraints:
-                  BoxConstraints(minWidth: 14, minHeight: 10),
+  Widget bodyWidget(){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Obx(() {
+          return AppBarWidget(
+              color: backgroundColor,
+              title: '${controller.userAddress.value.locality ?? ''}');
+        }),
+        // SearchEventDialog(),
+        InkWell(
+          onTap: () {
+            Get.dialog(SearchEventDialog());
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FormBuilderTextField(
+              enabled: false,
+              name: 'search',
+              // controller: controller.emailTextEditingController,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              keyboardType: TextInputType.emailAddress,
+              autofillHints: [AutofillHints.name],
+              decoration: InputDecoration(
+                hintMaxLines: 2,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
                 ),
-                validator: FormBuilderValidators.compose(
-                  [
-                    FormBuilderValidators.required(),
-                    FormBuilderValidators.email()
-                  ],
+                filled: true,
+                hintStyle: normalText.copyWith(height: 1.2
+                  //You can set your custom height here
+
                 ),
+                hintText: "Search All events...\nWhere | When | Category",
+                fillColor: Colors.white,
+                prefixIcon: IconButton(
+                    onPressed: null,
+                    icon: SvgPicture.asset(SearchIconTextFieldIconImage)),
+                suffixIcon: IconButton(
+                    onPressed: null,
+                    icon: SvgPicture.asset(filterTextFieldIconImage)),
+                prefixIconConstraints:
+                BoxConstraints(minWidth: 14, minHeight: 10),
+              ),
+              validator: FormBuilderValidators.compose(
+                [
+                  FormBuilderValidators.required(),
+                  FormBuilderValidators.email()
+                ],
               ),
             ),
           ),
-          CarouselSlider(
-            items: [
-              Image.asset('assets/svg/Rectangle_33.png', fit: BoxFit.fill),
-              Image.asset('assets/svg/Rectangle_33.png', fit: BoxFit.fill),
-              Image.asset('assets/svg/Rectangle_33.png', fit: BoxFit.fill),
-            ],
-            options: CarouselOptions(
-              height: 150,
-              viewportFraction: 0.95,
-              initialPage: 0,
-              enableInfiniteScroll: true,
-              reverse: false,
-              autoPlay: true,
-              autoPlayInterval: Duration(seconds: 3),
-              autoPlayAnimationDuration: Duration(milliseconds: 800),
-              autoPlayCurve: Curves.fastOutSlowIn,
-              enlargeCenterPage: false,
-              enlargeFactor: 0.8,
-              onPageChanged: (num, str) {
-                controller.currentSlide = num;
-              },
-              scrollDirection: Axis.horizontal,
-            ),
+        ),
+        CarouselSlider(
+          items: [
+            Image.asset('assets/svg/Rectangle_33.png', fit: BoxFit.fill),
+            Image.asset('assets/svg/Rectangle_33.png', fit: BoxFit.fill),
+            Image.asset('assets/svg/Rectangle_33.png', fit: BoxFit.fill),
+          ],
+          options: CarouselOptions(
+            height: 150,
+            viewportFraction: 0.95,
+            initialPage: 0,
+            enableInfiniteScroll: true,
+            reverse: false,
+            autoPlay: true,
+            autoPlayInterval: Duration(seconds: 3),
+            autoPlayAnimationDuration: Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enlargeCenterPage: false,
+            enlargeFactor: 0.8,
+            onPageChanged: (num, str) {
+              controller.currentSlide = num;
+            },
+            scrollDirection: Axis.horizontal,
           ),
-          SizedBox(height: 15,),
-          _buildDotsIndicator(controller.currentSlide, 3),
-          SizedBox(
-            height: 100,
-            child: ListView.builder(
-              physics: AlwaysScrollableScrollPhysics(),
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: controller.items.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Chip(
-                    avatar: Icon(Icons.star, color: textColor),
-                    label: Text(
-                      '${controller.items[index]}',
-                      style: normalText,
-                    ),
-                    backgroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      side: BorderSide(color: Colors.black, width: .2),
-                    ),
+        ),
+        SizedBox(height: 15,),
+        _buildDotsIndicator(controller.currentSlide, 3),
+        SizedBox(
+          height: 100,
+          child: ListView.builder(
+            physics: AlwaysScrollableScrollPhysics(),
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemCount: controller.items.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Chip(
+                  avatar: Icon(Icons.star, color: textColor),
+                  label: Text(
+                    '${controller.items[index]}',
+                    style: normalText,
                   ),
-                );
-              },
-            ),
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    side: BorderSide(color: Colors.black, width: .2),
+                  ),
+                ),
+              );
+            },
           ),
-          GetX(
-            init: HomeController(),
-            builder: (controller) {
-              if (controller.loading.value == true) {
-                return Center(child: Text('Loading ...'));
+        ),
+        GetX(
+          init: HomeController(),
+          builder: (controller) {
+            if (controller.loading.value == true) {
+              return Center(child: Text('Loading ...'));
+            } else {
+              if (controller.apiResponseModel.value.results == null) {
+                return Center(child: Text('No Data'));
               } else {
-                    if (controller.apiResponseModel.value.results == null) {
-                      return Center(child: Text('No Data'));
-                    } else {
                 return ListView.builder(
                   shrinkWrap: true,
                   physics: NeverScrollableScrollPhysics(),
@@ -148,22 +172,21 @@ class EventTab extends GetView<HomeController> {
                   itemCount:3,
                   itemBuilder: (context, index) {
                     Results result =
-                        controller.apiResponseModel.value.results![index];
+                    controller.apiResponseModel.value.results![index];
                     return SingleEventItemWidget(
                       result: result,
                       onPressed: () {
-                         HomeController.to
-                             .goToEventDetailsPage(uid: result.uid);
+                        HomeController.to
+                            .goToEventDetailsPage(uid: result.uid);
                       },
                     );
                   },
                 );
               }
-              }
-            },
-          ),
-        ],
-      ),
+            }
+          },
+        ),
+      ],
     );
   }
 
@@ -188,5 +211,12 @@ class EventTab extends GetView<HomeController> {
         );
       }),
     );
+  }
+
+  //responsive widget will do separate the UI based the api response
+  Widget responsiveWidget({required bool isScroll, required Widget child}){
+      return isScroll ? SingleChildScrollView(
+        child: child,
+      ) :  child;
   }
 }
