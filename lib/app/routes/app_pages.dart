1b28b093e01709f45fd61c57dd6498cf2dd9f130
svc <sathyayadav27@gmail.com>
2023-10-31 23:35:58 +0530

    ticket sales screen created

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -37,7 +37,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.SPLASH;
+  static const INITIAL = Routes.MyEvents;
 
   static final routes = [
     GetPage(
